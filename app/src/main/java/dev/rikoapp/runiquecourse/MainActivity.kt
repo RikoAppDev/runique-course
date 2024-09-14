335457704503938fc73becf72b5bce8e4aba2424
RikoAppDev <riko.appdev@gmail.com>
2024-09-15 00:26:11 +0200

    setting up custom material theme

--- app/src/main/java/dev/rikoapp/runiquecourse/MainActivity.kt
+++ app/src/main/java/dev/rikoapp/runiquecourse/MainActivity.kt
@@ -11,7 +11,7 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
-import dev.rikoapp.runiquecourse.ui.theme.RuniqueCourseTheme
+import dev.rikoapp.core.presentation.designsystem.RuniqueCourseTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
