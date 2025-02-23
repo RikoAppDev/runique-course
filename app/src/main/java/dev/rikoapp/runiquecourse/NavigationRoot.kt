3e9939492ff391406d44883d4088b76915487af5
RikoAppDev <riko.appdev@gmail.com>
2025-02-23 21:21:20 +0100

    notification foreground service

--- app/src/main/java/dev/rikoapp/runiquecourse/NavigationRoot.kt
+++ app/src/main/java/dev/rikoapp/runiquecourse/NavigationRoot.kt
@@ -1,15 +1,18 @@
 package dev.rikoapp.runiquecourse
 
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.platform.LocalContext
 import androidx.navigation.NavGraphBuilder
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.navigation
+import androidx.navigation.navDeepLink
 import dev.rikoapp.auth.presentation.intro.IntroScreenRoot
 import dev.rikoapp.auth.presentation.login.LoginScreenRoot
 import dev.rikoapp.auth.presentation.register.RegisterScreenRoot
 import dev.rikoapp.run.presentation.active_run.ActiveRunScreenRoot
+import dev.rikoapp.run.presentation.active_run.service.ActiveRunService
 import dev.rikoapp.run.presentation.run_overview.RunOverviewScreenRoot
 
 @Composable
@@ -91,8 +94,31 @@ private fun NavGraphBuilder.runGraph(navController: NavHostController) {
                 }
             )
         }
-        composable("active_run") {
-            ActiveRunScreenRoot()
+        composable(
+            route = "active_run",
+            deepLinks = listOf(
+                navDeepLink {
+                    uriPattern = "runique://active_run"
+                }
+            )
+        ) {
+            val context = LocalContext.current
+            ActiveRunScreenRoot(
+                onServiceToggle = { shouldServiceRun ->
+                    if (shouldServiceRun) {
+                        context.startService(
+                            ActiveRunService.createStartIntent(
+                                context = context,
+                                activityClass = MainActivity::class.java
+                            )
+                        )
+                    } else {
+                        context.startService(
+                            ActiveRunService.createStopIntent(context = context)
+                        )
+                    }
+                }
+            )
         }
     }
 }
--- run/presentation/src/main/java/dev/rikoapp/run/presentation/active_run/ActiveRunScreen.kt
+++ run/presentation/src/main/java/dev/rikoapp/run/presentation/active_run/ActiveRunScreen.kt
@@ -35,6 +35,7 @@ import dev.rikoapp.core.presentation.designsystem.components.RuniqueToolbar
 import dev.rikoapp.run.presentation.R
 import dev.rikoapp.run.presentation.active_run.components.RunDataCard
 import dev.rikoapp.run.presentation.active_run.maps.TrackerMap
+import dev.rikoapp.run.presentation.active_run.service.ActiveRunService
 import dev.rikoapp.run.presentation.util.hasLocationPermission
 import dev.rikoapp.run.presentation.util.hasNotificationPermission
 import dev.rikoapp.run.presentation.util.shouldShowLocationPermissionRationale
@@ -43,10 +44,12 @@ import org.koin.androidx.compose.koinViewModel
 
 @Composable
 fun ActiveRunScreenRoot(
+    onServiceToggle: (isServiceRunning: Boolean) -> Unit,
     viewModel: ActiveRunViewModel = koinViewModel()
 ) {
     ActiveRunScreen(
         state = viewModel.state,
+        onServiceToggle = onServiceToggle,
         onAction = viewModel::onAction
     )
 }
@@ -54,6 +57,7 @@ fun ActiveRunScreenRoot(
 @Composable
 private fun ActiveRunScreen(
     state: ActiveRunState,
+    onServiceToggle: (isServiceRunning: Boolean) -> Unit,
     onAction: (ActiveRunAction) -> Unit
 ) {
     val context = LocalContext.current
@@ -105,6 +109,18 @@ private fun ActiveRunScreen(
 
         if (!showLocationRationale && !showNotificationRationale) {
             permissionLauncher.requestRuniquePermissions(context)
+        }
+    }
+
+    LaunchedEffect(key1 = state.isRunFinished) {
+        if (state.isRunFinished) {
+            onServiceToggle(false)
+        }
+    }
+
+    LaunchedEffect(key1 = state.shouldTrack) {
+        if (context.hasLocationPermission() && state.shouldTrack && !ActiveRunService.isServiceActive) {
+            onServiceToggle(true)
         }
     }
 
@@ -254,6 +270,7 @@ private fun ActiveRunScreenPreview() {
     RuniqueCourseTheme {
         ActiveRunScreen(
             state = ActiveRunState(),
+            onServiceToggle = {},
             onAction = {}
         )
     }
