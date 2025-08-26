972132d6043ff3d32d9dbc756818925a8b94387a
RikoAppDev <riko.appdev@gmail.com>
2025-08-26 16:42:49 +0200

    test: user data validator

--- auth/domain/src/test/kotlin/dev/rikoapp/auth/domain/UserDataValidatorTest.kt
+++ auth/domain/src/test/kotlin/dev/rikoapp/auth/domain/UserDataValidatorTest.kt
@@ -4,6 +4,8 @@ import assertk.assertThat
 import assertk.assertions.isEqualTo
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
 
 class UserDataValidatorTest {
     private lateinit var userDataValidator: UserDataValidator
@@ -19,12 +21,17 @@ class UserDataValidatorTest {
         )
     }
 
-    @Test
-    fun `validatePassword returns true for a valid password`() {
-        val password = "Test12345"
-
+    @ParameterizedTest
+    @CsvSource(
+        "Test12345, true",
+        "test12345, false",
+        "12345, false",
+        "Test-1234, true",
+        "TEST12345, false"
+    )
+    fun testValidatePassword(password: String, expectedIsValid: Boolean) {
         val state = userDataValidator.validatePassword(password)
 
-        assertThat(state.isValidPassword).isEqualTo(true)
+        assertThat(state.isValidPassword).isEqualTo(expectedIsValid)
     }
 }
